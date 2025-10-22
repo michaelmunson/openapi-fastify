b367e69a974954ba1cde295987eb884cf775d439
Michael Munson <michael.s.munson@outlook.com>
2025-10-22 07:33:21 -0400

    chore: release 1.12.0 -> minor

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 export type * as OpenAPI from './openapi.types';
 export type * as Router from './router.types';
 export type * as FromSpec from './fromSpec.types';
+export type {SchemaToType} from './utils.types';
--- src/types/openapi.types.ts
+++ src/types/openapi.types.ts
@@ -12,17 +12,3 @@ export type RequestBody = OpenAPIV3.RequestBodyObject;
 export type Components = OpenAPIV3.ComponentsObject;
 export type Schema = OpenAPIV3.SchemaObject;
 export type Response = OpenAPIV3.ResponseObject;
-
-const x:Operator = {
-  responses: {
-    200: {
-      description: "User object",
-      content: {
-        "application/json": {
-          schema: { type: "object", properties: { id: { type: "integer" }, username: { type: "string" }, email: { type: "string" }, role: { type: "string" } }, 
-          required: ["id", "username", "email", "role"] as readonly string[] }
-        }
-      }
-    }
-  }
-}
